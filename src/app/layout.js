92dbe16e7e0ec80205c088f99430e69194ccf790
nikita <nikitaevtefeev@gmail.com>
2024-07-06 05:56:46 +0300

    fix ga

--- src/app/layout.js
+++ src/app/layout.js
@@ -1,9 +1,7 @@
 import "./globals.css";
 import Header from "../components/Header";
 import Footer from "../components/Footer";
-import ReactGA from "react-ga4";
 
-ReactGA.initialize("G-PCYEZBYF9D");
 
 
 
--- src/app/page.js
+++ src/app/page.js
@@ -1,12 +1,19 @@
 "use client"
 import '@/app/globals.css'; // Assuming the styles are moved to App.css
 import TimersPage from '@/components/Tokens';
+import ReactGA from "react-ga4";
 
+const TRACKING_ID = "G-XXXXXXXXX";
 
 const App = () => {
+  useEffect(() => {
+    ReactGA.initialize(TRACKING_ID);
+    // Send pageview with a custom path
+    ReactGA.send({ hitType: "pageview", page: "/", title: "Timers Page" });
+  }, [])
   return (
     <>
-    <TimersPage />
+      <TimersPage />
     </>
   );
 
